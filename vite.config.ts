4c1a841641d1177e2fcaedef07daf2e0055429cf
salvadorquinn <salvadorquinn2049@gmail.com>
2025-05-05 16:59:36 +0600

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig ({
-  
-  plugins: [react(),],
-  base: '/study-net-webspark',
+export default defineConfig({
+  base: '/study-net-webspark/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
